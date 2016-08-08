dd4e4e93ff455c7002a432fc5b3a7b9062c03768
Erik Sutherland <erik.misu@gmail.com>
2016-08-08 12:03:32 -0700

    more testing done

--- spec/atom-score-board-spec.js
+++ spec/atom-score-board-spec.js
@@ -1,47 +1,39 @@
 'use babel';
 
 import AtomScoreBoard from '../lib/atom-score-board'
+import { Rule } from '../lib/atom-score-rules'
 
-let board, fakeEntry;
+let board, fakeRule;
 
 describe('A score keeper', function () {
   beforeEach(function () {
-    board = new AtomScoreBoard()
+    board     = new AtomScoreBoard()
+    fakeEntry = new Rule('j', 'vim-mode:move-down', -1)
+    fakeEntry.resetTime()
+    board.addEntry(fakeEntry)
+  });
 
+  describe('The initial state', function () {
+    it('should be set', function () {
+      expect(board.score()).toEqual(fakeEntry.points)
+      expect(board.apm()).toEqual(2)
+      expect(board.history()).toEqual({'vim-mode:move-down': 1})
+    });
   });
 
   describe('when asked to reset', function () {
     beforeEach(function () {
-      board.state.score = 5
-      board.state.apmList = [{}]
-      board.state.history = {
-        'vim-mode:move-up': 5
-      }
       board.reset()
     });
-
     it('should reset the score', function () {
       expect(board.score()).toEqual(0)
     });
-
     it('should reset the apm', function () {
       expect(board.apm()).toEqual(0)
     });
-
     it('should not reset the history', function () {
-      expect(board.history()).toEqual()
+      expect(board.history()).toEqual({})
     });
   });
 
-  describe('when adding events', function () {
-    var fakeEvent;
-
-    beforeEach(function () {
-      fakeEvent = {
-        command: 'vim-mode:move-up',
-        points: 5,
-      }
-    });
-
-  });
 });
