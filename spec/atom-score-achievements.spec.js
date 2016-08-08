c2ecf92a2b6ba3d13566f37359a4f9747db4b834
Erik Sutherland <erik.misu@gmail.com>
2016-08-07 23:40:37 -0700

    stuble around test setup

--- spec/atom-score-achievements.spec.js
+++ spec/atom-score-achievements.spec.js
@@ -1,10 +1,18 @@
 'use babel';
 
+import AtomScoreAchievements from '../src/atom-score-achievements'
+
+var achievements
 
 describe('achievements', function () {
-  describe('criteria functions', function () {
-    it('apmAt', function () {
+  beforeEach(function () {
+    achievements = new AtomScoreAchievements()
+  });
 
+  describe('criteria functions', function () {
+    beforeEach(function () {
+    });
+    it('apmAt', function () {
     });
   });
 });
