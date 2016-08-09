efb90116ed6ab9119b69bb24f37f3ffe7e3e6e7f
Erik Sutherland <erik.misu@gmail.com>
2016-08-09 00:12:13 -0700

    add tests for new structures

---
+++ spec/atom-score-command-queue-spec.js
@@ -0,0 +1,7 @@
+'use babel'
+
+import AtomScoreCommandQueue from '../atom-score-command-queue'
+
+describe('A queue for keeping track of commands', function () {
+
+});
---
+++ spec/atom-score-history-map-spec.js
@@ -0,0 +1,7 @@
+'use babel';
+
+import AtomScoreHistoryMap from '../atom-score-history-map'
+
+describe('A map for keeping track of history information', function () {
+
+});
