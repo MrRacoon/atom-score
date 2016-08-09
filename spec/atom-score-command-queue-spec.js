a0d04f8384a5cc79216a30879328ea47c8a268f7
Erik Sutherland <erik.misu@gmail.com>
2016-08-09 00:24:06 -0700

    adding moar tests

--- spec/atom-score-command-queue-spec.js
+++ spec/atom-score-command-queue-spec.js
@@ -1,7 +1,49 @@
 'use babel'
 
-import AtomScoreCommandQueue from '../atom-score-command-queue'
+import AtomScoreCommandQueue from '../lib/atom-score-command-queue'
+import { Rule } from '../lib/atom-score-rules'
+
+const ARBITRARY = 'deadbeef'
 
 describe('A queue for keeping track of commands', function () {
-
+  var sut, fakeEvent
+  describe('on initialization', function () {
+    describe('using no argument', function () {
+      beforeEach(function () {
+        sut = new AtomScoreCommandQueue()
+        console.log(sut)
+      });
+      it('should have an empty queue', function () {
+        expect(sut.queue.length).toEqual(0)
+      });
+    });
+    describe('using no argument', function () {
+      beforeEach(function () {
+        sut = new AtomScoreCommandQueue([ARBITRARY])
+        console.log(sut)
+      });
+      it('should have an empty queue', function () {
+        expect(sut.queue.length).toEqual(1)
+      });
+    });
+  });
+  describe('After initialization', function () {
+    beforeEach(function () {
+      sut = new AtomScoreCommandQueue()
+      fakeEvent = new Rule('j', 'vim-mode:move-down', -1)
+    });
+    describe('when adding an event', function () {
+      beforeEach(function () {
+        sut.add(fakeEvent)
+      });
+      describe('the queue', function () {
+        it('should not be empty', function () {
+          expect(sut.queue.length).toEqual(1)
+        });
+        it('should contain the event passed in', function () {
+          expect(sut.queue).toEqual([fakeEvent])
+        });
+      });
+    });
+  });
 });
--- spec/atom-score-history-map-spec.js
+++ spec/atom-score-history-map-spec.js
@@ -1,6 +1,6 @@
 'use babel';
 
-import AtomScoreHistoryMap from '../atom-score-history-map'
+import AtomScoreHistoryMap from '../lib/atom-score-history-map'
 
 describe('A map for keeping track of history information', function () {
 
