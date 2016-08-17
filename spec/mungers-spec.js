369191600ff99fdb1acc2a73a22c6a9a2af7b0f2
Erik Sutherland <erik.misu@gmail.com>
2016-08-16 23:34:55 -0700

    add 'last' command combo used

--- spec/mungers-spec.js
+++ spec/mungers-spec.js
@@ -1,11 +1,20 @@
 'use babel'
 
-import { getScore, getCount } from '../lib/mungers'
+import { getScore, getCount, lastCommandName } from '../lib/mungers'
 import rootReducer            from '../lib/rootReducer'
-import { score, counter, history } from '../lib/state'
+import { score, counter, history, last } from '../lib/state'
 
+const STRING = 'Han solo'
+const CHAR   = 'T'
+
+const someStim  = {
+  id: STRING,
+  combo: CHAR
+}
+
+const lastCommand     = last.actions.set(someStim)
 const add5Points      = score.actions.add(5)
-const addHistoryCount = history.actions.increment(5)
+const addHistoryCount = history.actions.increment(someStim)
 const addOneCount     = counter.actions.increment()
 
 var state
@@ -29,6 +38,13 @@ describe('mungers to read the state', function () {
     it('should return the number of command tracked', function () {
       expect(getCount(state)).toEqual(1)
       expect(getCount(rootReducer(state, addOneCount))).toEqual(2)
+    })
+  })
+  describe('lastCommandName', function () {
+    it('should store the last stimulus that triggered the reducer', function () {
+      expect(lastCommandName(state)).toEqual(undefined)
+      const commandAdded = rootReducer(state, lastCommand)
+      expect(lastCommandName(commandAdded)).toEqual(CHAR)
     })
   })
 })
