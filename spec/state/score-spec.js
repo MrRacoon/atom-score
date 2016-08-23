3ae7bc40805afd6168e9111953d906f9532dd751
Erik Sutherland <erik.misu@gmail.com>
2016-08-22 18:48:14 -0700

    fix lint errors

--- spec/state/score-spec.js
+++ spec/state/score-spec.js
@@ -31,7 +31,6 @@ describe('The score tracker', function () {
     });
     describe('when given an add message', function () {
       it('should add the amount to the score property of the state', function () {
-        var amount = 1;
         score.reducer({}, types.actions.add({points: 7})).should.eql({
           [score.constants.NAME]: 7
         });
@@ -40,7 +39,7 @@ describe('The score tracker', function () {
     describe('when given the reset method', function () {
       it('should reset the score to zero', function () {
         const state = score.reducer({score: 100}, types.actions.reset());
-        state.score.should.be.eql(0)
+        state.score.should.be.eql(0);
       });
     });
   });
